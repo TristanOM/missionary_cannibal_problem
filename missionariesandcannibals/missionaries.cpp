0974ed07ccf842b3728a1d212738f197be2ce53c
Tristan <tristanolivermall212@pointloma.edu>
2016-02-22 18:35:21 -0800

    Missionary_Cannibal_Problem

---
+++ missionariesandcannibals/missionaries.cpp
@@ -0,0 +1,20 @@
+//Missionaries and Cannibals problem
+//Tristan Oliver-Mallory
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main()
+{
+	//The left side of the array represents the number of missionaries and right represent number of cannibals
+	int rightSide [2] = {3,3};//This is the starting side
+	int leftSide [2] = {0,0};//this is the finishing side
+	
+	int boat [2] = {0,0};//this is the boat
+
+	
+}
